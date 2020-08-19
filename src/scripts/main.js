1686d6171ebb3dd25a4e4e6c7438c62f0c7de6e7
Ricky Blake <rab0196@yahoo.com>
2020-08-19 17:04:54 -0500

    tilled fields

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,36 @@
 import { createPlan } from './plan.js'
+import { createAsparagus } from "./seeds/asparagus.js"
+import { createCorn } from "./seeds/corn.js"
+import { createPotato } from "./seeds/potato.js"
+import { createSoybean } from "./seeds/soybean.js"
+import { createSunflower } from "./seeds/sunflower.js"
+import { createWheat } from "./seeds/wheat.js"
+import { addPlant } from "./field.js"
+import { usePlants } from "./field.js"
 
 const yearlyPlan = createPlan();
-
 console.log(yearlyPlan)
 
+const asparagusSeed = createAsparagus()
+console.log(asparagusSeed)
+
+const cornSeed = createCorn()
+console.log(cornSeed)
+
+const potatoSeed = createPotato()
+console.log(potatoSeed)
+
+const soybeanSeed = createSoybean()
+console.log(soybeanSeed)
+
+const sunflowerSeed = createSunflower()
+console.log(sunflowerSeed)
+
+const wheatSeed = createWheat()
+console.log(wheatSeed)
+
+const seed = cornSeed;
+
+addPlant(seed);
+const field = usePlants();
+console.log(field);
