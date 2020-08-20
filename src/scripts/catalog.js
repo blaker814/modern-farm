e5cfb4c625bdaac79668f73560f2d0a0b1ebe30f
Ricky Blake <rab0196@yahoo.com>
2020-08-20 13:29:57 -0500

    seperated each plant and gave number of

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -1,6 +1,19 @@
 export const Catalog = (foodArray) => {
     const foodHTMLRep = document.querySelector("main")
-    for (const food of foodArray) {
-        foodHTMLRep.innerHTML += `<section class="plant">${food.type}</section>`
+    foodArray.sort(function(a, b) {
+        return a.type.localeCompare(b.type)
+     })
+
+     const occurrences = {};
+     for (let i = 0; i<foodArray.length; i++) {
+        occurrences[foodArray[i].type] = (occurrences[foodArray[i].type] || 0) + 1;
+     }
+     
+     console.log(occurrences); 
+    for (const [food, num] of Object.entries(occurrences)) {
+        foodHTMLRep.innerHTML += `<section class="plant">
+        <h3>${food}</h3> 
+            <p>${num}</p>
+        </section>`
     }
 }
