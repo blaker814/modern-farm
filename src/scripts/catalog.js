966e29ffabe4a398a43517ef1c1185c3c5acda10
Ricky Blake <rab0196@yahoo.com>
2020-08-20 10:52:56 -0500

    sorted plants

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -1,5 +1,9 @@
 export const Catalog = (foodArray) => {
     const foodHTMLRep = document.querySelector("main")
+    foodArray.sort(function(a, b) {
+       return a.type.localeCompare(b.type)
+    })
+    
     for (const food of foodArray) {
         foodHTMLRep.innerHTML += `<section class="plant">${food.type}</section>`
     }
